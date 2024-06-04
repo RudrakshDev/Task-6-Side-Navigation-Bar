bbd2377db711dd473a23966380362e0d9cb5f4e3
RudrakshDev <wis40311@gmail.com>
2024-06-03 20:11:47 -0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const sidebarLinks = document.querySelectorAll('.sidebar a');
-
-sidebarLinks.forEach(link => {
-  link.addEventListener('click', () => {
-  
-    sidebarLinks.forEach(link => link.classList.remove('active'));
-    link.classList.add('active');
-  });
-});
